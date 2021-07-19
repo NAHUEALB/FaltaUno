fdc2917d27d97afc8bdd9d3c821730b7a0aede0a
Nahuel <nahuelalbertiutn@gmail.com>
2021-07-19 15:57:13 -0300

    Correccion de navegacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
 import { BuscarPage } from './pages/buscar/buscar.page';
 import { InicioPage } from './pages/inicio/inicio.page';
 import { PerfilPage } from './pages/perfil/perfil.page';
@@ -13,21 +14,34 @@ import { PerfilPage } from './pages/perfil/perfil.page';
 export class AppComponent {
 
   options: Array<{ title: string, component: any, icon: string, ruta:string}>;
+    constructor(
+      private platform: Platform,
+      private router: Router
+    ) {
+      this.router.navigate(["perfil"]);
+      this.options = [
+        { title: 'Inicio', component: InicioPage, icon:'' ,  ruta:'inicio' },
+        { title: 'Buscar', component: BuscarPage, icon: '',  ruta: 'buscar' },
+        { title: 'Perfil', component: PerfilPage, icon:'',  ruta: 'perfil' }
+      ]
+      console.log("iniciar app");
+      this.initializeApp();
+    }
+  
+    initializeApp() {
+      console.log("inicie app");
+      this.platform.ready().then(() => {
+        console.log("tamo ready");
+      });
+    }
 
-  constructor(private router: Router) {
 
-    this.options = [
-      { title: 'Inicio', component: InicioPage, icon:'' ,  ruta:'inicio' },
-      { title: 'Buscar', component: BuscarPage, icon: '',  ruta: 'buscar' },
-      { title: 'Perfil', component: PerfilPage, icon:'',  ruta: 'perfil' }
-    ]
-  }
+
 
   openOptions(option){
     console.log(option);
     console.log("me voy a ir"+option.ruta);
-    // this.router.navigate(['buscar']);
-    this.router.navigate([`${option.ruta}`]);
+    this.router.navigate([`/${option.ruta}`]);
   }
 
 }
