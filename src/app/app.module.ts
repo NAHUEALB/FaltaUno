59479ab746b2d97425271d79e2de23a6ba233e6b
@nsandobal <nikosanarg@gmail.com>
2021-08-18 20:38:41 -0300

    Agregar modulo de Firebase para Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,22 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+	  BrowserModule, 
+	  IonicModule.forRoot(), 
+	  AppRoutingModule,
+	  AngularFireModule.initializeApp(environment.firebaseConfig)
+	],
+  providers: [{ 
+	  provide: RouteReuseStrategy, 
+	  useClass: IonicRouteStrategy 
+  }],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent],
 })
