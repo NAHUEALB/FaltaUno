1d4fce5d73688fc4d490ecc50f3ca17d18d52cac
@nsandobal <nikosanarg@gmail.com>
2021-08-19 18:49:54 -0300

    Recuperar previo a merge

--- src/app/models/interfaces.ts
+++ src/app/models/interfaces.ts
@@ -1,15 +1,11 @@
 export interface Jugador {
 	nombre: string; // validar que no tenga insultos (?
 	usuario: string; // se almacena sin el "@"
-	fnacimiento: Date; // debe ser compatible con el timestamp de Firebase
+	fnacimiento: string; // debe ser compatible con el timestamp de Firebase
 	puntaje: number; // acumulador para los votos
 	cvotos: number; // contador de votos totales
-	sexo: Sexo;
+	sexo: string;
 	perfil: boolean; // true: Público; false: Privado (sugerir nombre más descriptivo)
 	foto: string; // guarda link?
-	ubicacion: Localidad;
+	ubicacion: string;
 }
-
-export type Sexo = 'No definido | Masculino | Femenino | No binario';
-
-export type Localidad = 'La Plata';
