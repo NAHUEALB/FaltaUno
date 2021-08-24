17effb1892ab93829f3bcd36d8adf6f4c8509762
@nsandobal <nikosanarg@gmail.com>
2021-08-24 14:18:26 -0300

    Cargar módulos necesarios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+	apiKey: "AIzaSyBwyUBCBgn39-u2a81EazNruO9n5vJtr7c",
+	authDomain: "fenixalpha1-0.firebaseapp.com",
+	projectId: "fenixalpha1-0",
+	storageBucket: "fenixalpha1-0.appspot.com",
+	messagingSenderId: "950961474171",
+	appId: "1:950961474171:web:27c6ada7d2d929c92430de",
+	measurementId: "G-54PJFGBF6H"
+  }
 };
