f71f58554ed8bac087e6fac83b9af9e5bcabd5ab
@nsandobal <nikosanarg@gmail.com>
2021-08-25 00:04:37 -0300

    Mensaje de éxito y cartel de loading al registrar

--- src/app/pages/registrar/registrar.page.ts
+++ src/app/pages/registrar/registrar.page.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { NavigationExtras, Router } from '@angular/router';
-import { MenuController } from '@ionic/angular';
+import { LoadingController, MenuController } from '@ionic/angular';
+
 import { Jugador } from 'src/app/models/jugador';
 import { DatabaseService } from 'src/app/serv/database.service';
+
+import { ToastController } from '@ionic/angular';
 
 @Component({
 	selector: 'app-registrar',
@@ -16,9 +19,15 @@ export class RegistrarPage implements OnInit {
 	newJugador: Jugador;
 
 	localidades = ["La Plata", "Berazategue", "Tu vieja"];
-	sexos = ["Elegir sexo...", "Hombre", "Mujer", "No binario"];
+	sexos = ["No especificado", "Hombre", "Mujer", "No binario"];
 
-	constructor(public formBuilder: FormBuilder, private router: Router, public menuCtrl: MenuController, public database: DatabaseService) { 
+	constructor(
+		public formBuilder: FormBuilder, 
+		private router: Router, 
+		public menuCtrl: MenuController, 
+		public database: DatabaseService,
+		public toastController: ToastController,
+		public loadingController: LoadingController) { 
 		// this.menuCtrl.enable(false, 'slideMenu');
 		this.newJugador = {
 			id: '',
@@ -46,22 +55,48 @@ export class RegistrarPage implements OnInit {
 		console.log("Vista de registrar cargada!");
 	}
 
-	onSubmit(){
+	async onSubmit(){
+		this.presentLoading();
 		this.newJugador.nombre = this.jugadorForm.value.nombre;
 		let jugadorExtra : NavigationExtras = {
 			state: {
 				jugador: this.newJugador
 			}
 		}
-		this.router.navigate(['inicio'], jugadorExtra);
-		
-		console.log("vamos a guardar esto: ");
-		console.log(this.newJugador);
-
-
+		//console.log("vamos a guardar esto: ");
+		//console.log(this.newJugador);
 		const data = this.newJugador;
 		data.id = this.database.createId();
 		const link = 'Jugadores';
-		this.database.createDocument<Jugador>(data, link, data.id);
+		await this.database.createDocument<Jugador>(data, link, data.id)
+		.then(() => {
+			this.presentToast("Perfil creado correctamente", 3000);
+			this.router.navigate(['inicio'], jugadorExtra);
+		})
+		.catch((err) => {
+			this.presentToast(err, 3000);
+			this.router.navigate(['principal'], jugadorExtra);
+		})
+			
 	}
+
+	async presentToast(msg: string, time: number) {
+		const toast = await this.toastController.create({
+			message: msg,
+			duration: time,
+		});
+		toast.present();
+	}
+
+	async presentLoading() {
+		const loading = await this.loadingController.create({
+			cssClass: 'my-custom-class',
+			message: 'Por favor, espere',
+			duration: 2000
+		});
+		await loading.present();
+	
+		const { role, data } = await loading.onDidDismiss();
+		console.log('Loading dismissed!');
+	  }
 }
