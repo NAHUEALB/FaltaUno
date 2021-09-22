69ecd0bec4be0fd8f2074462853c01e1e4fe412d
Nikos90 <nikosanarg@gmail.com>
2021-09-22 16:19:34 -0300

    Estructura base con error en consola

--- src/app/pages/buscar/buscar.page.ts
+++ src/app/pages/buscar/buscar.page.ts
@@ -1,17 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-buscar',
-  templateUrl: './buscar.page.html',
-  styleUrls: ['./buscar.page.scss'],
+	selector: 'app-buscar',
+	templateUrl: './buscar.page.html',
+	styleUrls: ['./buscar.page.scss'],
 })
 export class BuscarPage implements OnInit {
 
-  constructor() {
-    console.log("Constructor del buscar");
-   }
+	partidos = [
+		{
+			cancha: "Megastadio",
+			direccion: "Calle 1 y 64",
+			slotsOcupados: 7,
+			slotsTotales: 10,
+			hora: "16:00",
+			sexo: "Masculino"
+		},
+		{
+			cancha: "Estadio 7",
+			direccion: "Calle 6 y 59",
+			slotsOcupados: 3,
+			slotsTotales: 10,
+			hora: "17:00",
+			sexo: "Mixto"
+		},
+		{
+			cancha: "Megastadio",
+			direccion: "Calle 1 y 64",
+			slotsOcupados: 5,
+			slotsTotales: 10,
+			hora: "17:00",
+			sexo: "Femenino"
+		},
+		{
+			cancha: "Cancha Loca",
+			direccion: "Calle 22 y Palermo",
+			slotsOcupados: 2,
+			slotsTotales: 10,
+			hora: "19:00",
+			sexo: "Masculino"
+		}
+	]
 
-  ngOnInit() {
-  }
+	constructor() {
+		console.log("Constructor del buscar");
+	}
+
+	ngOnInit() {
+	}
 
 }
