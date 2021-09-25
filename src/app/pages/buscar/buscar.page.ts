ed298fe3e8df4fac4512a3f56de3f41927a4f7ba
Nikos90 <nikosanarg@gmail.com>
2021-09-25 11:21:17 -0300

    Mejora visual page buscar

--- src/app/pages/buscar/buscar.page.ts
+++ src/app/pages/buscar/buscar.page.ts
@@ -17,7 +17,7 @@ export class BuscarPage implements OnInit {
 			direccion: "Calle 1 y 64",
 			slotsOcupados: 7,
 			slotsTotales: 10,
-			hora: "16:00",
+			hora: "12:00",
 			sexo: " Masculino ",
 		},
 		{
@@ -25,7 +25,7 @@ export class BuscarPage implements OnInit {
 			direccion: "Calle 6 y 59",
 			slotsOcupados: 3,
 			slotsTotales: 10,
-			hora: "17:00",
+			hora: "12:00",
 			sexo: " Mixto "
 		},
 		{
@@ -33,7 +33,7 @@ export class BuscarPage implements OnInit {
 			direccion: "Calle 1 y 64",
 			slotsOcupados: 9,
 			slotsTotales: 10,
-			hora: "17:00",
+			hora: "13:00",
 			sexo: " Femenino "
 		},
 		{
@@ -41,17 +41,15 @@ export class BuscarPage implements OnInit {
 			direccion: "Calle 22 y Palermo",
 			slotsOcupados: 9,
 			slotsTotales: 10,
-			hora: "19:00",
+			hora: "13:00",
 			sexo: " Masculino "
 		},
-
-
 		{
 			cancha: "Cancha La Lora",
 			direccion: "Calle 44 y 5",
 			slotsOcupados: 6,
 			slotsTotales: 10,
-			hora: "19:00",
+			hora: "13:00",
 			sexo: " Masculino "
 		},
 		{
@@ -59,7 +57,7 @@ export class BuscarPage implements OnInit {
 			direccion: "Calle 44 y 5",
 			slotsOcupados: 5,
 			slotsTotales: 10,
-			hora: "20:00",
+			hora: "14:00",
 			sexo: " Femenino "
 		},
 		{
@@ -67,7 +65,7 @@ export class BuscarPage implements OnInit {
 			direccion: "Calle 1 y 64",
 			slotsOcupados: 9,
 			slotsTotales: 10,
-			hora: "20:00",
+			hora: "14:00",
 			sexo: " Mixto "
 		},
 		{
@@ -75,27 +73,107 @@ export class BuscarPage implements OnInit {
 			direccion: "Calle 6 y 59",
 			slotsOcupados: 8,
 			slotsTotales: 10,
-			hora: "21:00",
+			hora: "14:00",
 			sexo: " Masculino "
-		}
-	]
+		},
+		{
+			cancha: "Megastadio",
+			direccion: "Calle 1 y 64",
+			slotsOcupados: 7,
+			slotsTotales: 10,
+			hora: "15:00",
+			sexo: " Mixto "
+		},
+		{
+			cancha: "Estadio 7",
+			direccion: "Calle 6 y 59",
+			slotsOcupados: 3,
+			slotsTotales: 10,
+			hora: "15:00",
+			sexo: " Mixto "
+		},
+		{
+			cancha: "Cancha Loca",
+			direccion: "Calle 22 y Palermo",
+			slotsOcupados: 9,
+			slotsTotales: 10,
+			hora: "15:00",
+			sexo: " Masculino "
+		},
+		{
+			cancha: "Megastadio",
+			direccion: "Calle 1 y 64",
+			slotsOcupados: 2,
+			slotsTotales: 10,
+			hora: "16:00",
+			sexo: " Masculino "
+		},
+		{
+			cancha: "Estadio 7",
+			direccion: "Calle 6 y 59",
+			slotsOcupados: 2,
+			slotsTotales: 10,
+			hora: "16:00",
+			sexo: " Mixto "
+		},
+		{
+			cancha: "Cancha Loca",
+			direccion: "Calle 22 y Palermo",
+			slotsOcupados: 7,
+			slotsTotales: 10,
+			hora: "16:00",
+			sexo: " Mixto "
+		},
+		{
+			cancha: "Megastadio",
+			direccion: "Calle 1 y 64",
+			slotsOcupados: 3,
+			slotsTotales: 10,
+			hora: "17:00",
+			sexo: " Mixto "
+		},
+		{
+			cancha: "Estadio 7",
+			direccion: "Calle 6 y 59",
+			slotsOcupados: 2,
+			slotsTotales: 10,
+			hora: "17:00",
+			sexo: " Femenino "
+		},
+		{
+			cancha: "Cancha La Lora",
+			direccion: "Calle 44 y 5",
+			slotsOcupados: 9,
+			slotsTotales: 10,
+			hora: "18:00",
+			sexo: " Femenino "
+		},
+	];
+
+	distancias = {
+		"Megastadio" : 3,
+		"Estadio 7" : 2,
+		"Cancha La Lora" : 6,
+		"Cancha Loca" : 5
+	}
 
 	constructor(private router: Router) {
-		console.log("Constructor del buscar");
 	}
 
 	ngOnInit() {
 		this.partidos.forEach(unPartido => {
-			this.asignarSexo(unPartido)
-			this.asignarColorSlot(unPartido)
+			this.setSexo(unPartido)
+			this.setColorSlot(unPartido)
+			this.setOrden(unPartido)
 		});
+		this.ordenarPartidos(this.partidos);
 	}
 
 	irAlInicio() {
 		this.router.navigate([`/inicio`]);
 	}
 
-	asignarSexo(elem) {
+	setSexo(elem) {
 		switch (elem.sexo) {
 			case " Masculino ": 
 				elem.iconName = "male-outline";
@@ -112,14 +190,30 @@ export class BuscarPage implements OnInit {
 		}
 	}
 	
-	asignarColorSlot(elem) {
+	setColorSlot(elem) {
+		elem.optionFlama = "";
 		if (elem.slotsOcupados < 5) {
 			elem.slotColor = "slot-disponible";
-		} else if (elem.slotsOcupados < 9) {
+		} else if (elem.slotsOcupados < 8) {
 			elem.slotColor = "slot-popular";
 		} else {
+			elem.optionFlama = "flama";
 			elem.slotColor = "slot-ocupado";
 		}
+	}
+
+	setOrden(elem) {
+		elem.orden = 0;
+		let now = new Date();
+		let horaNow = now.getHours();
+		let horaPartido = elem.hora.split(":")[0] - 0;
+		elem.orden += Math.floor(Math.max(0, horaPartido - horaNow)/2);
+		elem.orden += (10 - elem.slotsOcupados) + 1;
+		elem.orden += Math.floor(this.distancias[elem.cancha]/2);
+	}
+
+	ordenarPartidos(partidos) {
+		partidos.sort(function(a, b){return a.orden-b.orden});
 	}
 
 	swapAMapa() {
